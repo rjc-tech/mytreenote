760a627767f9d216afdab797e7d9c423cd7764a4
RJC\s_kabamoto <rrrtestjc@gmail.com>
2017-06-13 18:56:22 +0900

    refs #12299 JSON形式のやりとりに変更、必要なファンクションの定義のみ作成

---
+++ www/lib/mytreenote.js
@@ -0,0 +1,47 @@
+/*
+ Mytreenote用ファンクション
+*/
+
+// 親topic作成処理
+function parentTopicCreate() {
+
+}
+
+// 子topic作成処理
+function childrenTopicCreate() {
+
+}
+
+
+// 開閉ボタン押下処理
+function OpenAndClose() {
+
+}
+
+
+
+// 削除ボタン押下処理
+function deleteTopic() {
+
+}
+
+
+
+// カレントボタン押下処理
+function currentMove() {
+
+}
+
+
+
+
+// カレントボタン(上)押下処理
+function hierarchyUpMove() {
+
+}
+
+
+// カレントボタン(下)押下処理
+function hierarchyUnderMove() {
+
+}
